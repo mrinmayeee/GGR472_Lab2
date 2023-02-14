a5dcd3fb1d8cf0dff8d168d9b3ae26277abd6573
mrinmayeee <91093423+mrinmayeee@users.noreply.github.com>
2023-02-14 16:47:49 -0500

    added comments to js
    couldn't add comments for html or css - find out why

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibXJpbm1heWVlZSIsImEiOiJjbGRtMHNobWkwMnRhM25teTJ6Y3poYWY3In0.7jz_b3HAoeEVcCmXB3qCKA'; //Add default public map token from your Mapbox account  
  
 const map = new mapboxgl.Map({ 
-    container: 'map', // Add div container ID for your map 
-    style: 'mapbox://styles/mrinmayeee/cle37p2v2001a01mu0e9w46ww', // Add link to style URL 
-    center: [-79.444135, 43.743110], // starting position [longitude, latitude] 
-    zoom: 9, // starting zoom 
+    container: 'map', // Adding div container ID from index.html file for my map 
+    style: 'mapbox://styles/mrinmayeee/cle37p2v2001a01mu0e9w46ww', // link to my style from Mapbox 
+    center: [-79.444135, 43.743110], // starting position for map [longitude, latitude] 
+    zoom: 10, // starting zoom 
 });
 
-map.addControl(new mapboxgl.FullscreenControl());
+map.addControl(new mapboxgl.FullscreenControl()); // adds a button in top-right corner to toggel betewen fullscreen and regular view
 
-map.on('load', () => {
-    map.addSource('toronto-renewables',{
-        type: 'vector',
-        url: 'mapbox://mrinmayeee.2kpnir4n',
+map.on('load', () => { // loads vector tileset from my Mapbox studio
+    map.addSource('toronto-renewables',{ // sets the unique source name
+        type: 'vector', // specifies the data type
+        url: 'mapbox://mrinmayeee.2kpnir4n', //url for the tileset in Mpabox studio using the tileset ID
     });
 
-    map.addLayer(
+    map.addLayer( //allows me to add how I want the layer to show up
         { 
-            'id': 'renewables_locations', 
-            'type': 'circle', 
+            'id': 'renewables_locations', // unique id for each layer
+            'type': 'circle', // circle since the data is individual points
             'source': 'toronto-renewables',
-            'source-layer': 'renewable-energy-installation-do3oc3',
-            'paint':  { 
+            'source-layer': 'renewable-energy-installation-do3oc3', // name of the layer in my mapbox studio
+            'paint':  { // gives the specific atrributes of the marker circles
                 'circle-radius': 6, 
                 'circle-color': '#efd215',
                 'circle-stroke-colour': '#1d1b1b' 
@@ -30,22 +30,20 @@ map.on('load', () => {
     );
 });
 
-map.on('load', () => {
+map.on('load', () => { // adding a second data source for the outline of GTA
     map.addSource('toronto-outline',{
         type: 'vector',
         url: 'mapbox://mrinmayeee.49wo0i57',
     });
 
-    map.addLayer(
+    map.addLayer( // same as above but for the 2nd layer
         { 
             'id': 'toronto', 
             'type': 'fill', 
             'source': 'toronto-outline',
             'source-layer': 'toronto-boundary-wgs84-31cf5b',
-            'paint':  { 
-                'fill-colour': 6, 
+            'paint':  {  // different attributes since this layer is a polygon 
                 'fill-color': '#c19149',
-                'circle-stroke-colour': '#1d1b1b' 
             } 
         },
     );
