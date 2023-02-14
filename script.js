b8761b984d40a2c88eec3271cf1ea33747e12205
mrinmayeee <91093423+mrinmayeee@users.noreply.github.com>
2023-02-14 15:58:26 -0500

    added rest

--- script.js
+++ script.js
@@ -5,26 +5,51 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/mrinmayeee/cle37p2v2001a01mu0e9w46ww', // Add link to style URL 
     center: [-79.444135, 43.743110], // starting position [longitude, latitude] 
     zoom: 9, // starting zoom 
-})
-map.on('load', () => { 
-    /*ADDING A SOURCE FROM A GEOJSON FILE*/ 
-    map.addSource('toronto_renewable', { 
-        type: 'geojson', 
-        data: 'https://studio.mapbox.com/tilesets/mrinmayeee.2kpnir4n'
-    });
-    
-    map.addLayer({ 
-    'id': 'renewables_locations', 
-    'type': 'circle', 
-    'source': 'toronto_renewable', 
-    'paint': { 
-        'circle-radius': 6, 
-        'circle-color': '#efd215',
-        'circle-stroke-colour': '#1d1b1b' 
-    } 
-    }); 
 });
 
+map.addControl(new mapboxgl.FullscreenControl());
 
+map.on('load', () => {
+    map.addSource('toronto-renewables',{
+        type: 'vector',
+        url: 'mapbox://mrinmayeee.2kpnir4n',
+    });
+
+    map.addLayer(
+        { 
+            'id': 'renewables_locations', 
+            'type': 'circle', 
+            'source': 'toronto-renewables',
+            'source-layer': 'renewable-energy-installation-do3oc3',
+            'paint':  { 
+                'circle-radius': 6, 
+                'circle-color': '#efd215',
+                'circle-stroke-colour': '#1d1b1b' 
+            } 
+        },
+    );
+});
+
+map.on('load', () => {
+    map.addSource('toronto-outline',{
+        type: 'vector',
+        url: 'mapbox://mrinmayeee.49wo0i57',
+    });
+
+    map.addLayer(
+        { 
+            'id': 'toronto', 
+            'type': 'fill', 
+            'source': 'toronto-outline',
+            'source-layer': 'toronto-boundary-wgs84-31cf5b',
+            'paint':  { 
+                'fill-colour': 6, 
+                'fill-color': '#c19149',
+                'circle-stroke-colour': '#1d1b1b' 
+            } 
+        },
+    );
+
+});
 
 
